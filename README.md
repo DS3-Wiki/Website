dd85d30e20abb59636b3e9dc78714041a5f46859
LccsBueno <125739142+LccsBueno@users.noreply.github.com>
2023-05-25 08:28:36 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Site_Institucional
+Repositório que contém tanto o design do site (figma) quanto o próprio site em si.
---
+++ site/src/controllers/bossController.js
@@ -0,0 +1,33 @@
+var avisoModel = require("../models/avisoModel");
+
+function testar(req, res) {
+    console.log("ENTRAMOS NO avisoController");
+    res.send("ENTRAMOS NO AVISO CONTROLLER");
+}
+
+function boss_pesquisar(req, res) {
+    var boss_name = req.params.descricao;
+
+    avisoModel.boss_pesquisar(boss_name)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+
+module.exports = {
+    pesquisarDescricao,
+    testar
+}
